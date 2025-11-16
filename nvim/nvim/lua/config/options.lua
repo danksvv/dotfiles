ecc440039b199e1b2f6fe8d7c03153d5a4246ee3
danksvv <dfvv29@gmail.com>
2025-11-16 19:44:28 +0100

    feat(vim): Ajusta la configuración de 'guicursor' para que el cursor parpadee también en modo Normal/navegación, manteniendo la distinción visual de la forma del cursor por modo.
    ### Archivo Modificado:
    * lua/config/options.lua
    
    ### Cambios realizados:
    
    * **Cursor en modo Normal (n-v-r-c-T):**
        * Se añade la opción de parpadeo (`blinkon500-blinkoff500`) al
    cursor de **bloque** (`block`) para que parpadee en modos de navegación.
    
    * **Cursor en modo Insertar (i):**
        * Se mantiene la barra vertical (`ver25`) y el parpadeo.
    
    * **Resultado:** El cursor parpadea en todos los modos, pero usa un
    **cuadrado parpadeante** en modo Normal y una **línea parpadeante** en
    modo Insertar, manteniendo la animación suave entre transiciones
    (gracias a `mini.animate`).

--- nvim/nvim/lua/config/options.lua
+++ nvim/nvim/lua/config/options.lua
@@ -2,4 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.guicursor = "n-v-r-c-T:block,i:ver25-blinkon500-blinkoff500"
+-- vim.opt.guicursor = "n-v-r-c-T:block,i:ver25-blinkon500-blinkoff500"
+-- Nuevo guicursor:
+-- n-v-r-c-T:block con parpadeo para los modos de navegación.
+-- i:ver25 con parpadeo para el modo de escritura (línea).
+
+vim.opt.guicursor = "n-v-r-c-T:block-blinkon500-blinkoff500,i:ver25-blinkon500-blinkoff500"
